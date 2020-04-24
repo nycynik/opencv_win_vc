34ee0559ea01d05f142cb4024512346ae516f316
Mike Lynch <nycynik@gmail.com>
2020-04-24 01:04:58 -0700

    Adding images and simple image reading code

--- opencv_win_vc/opencv_win_vc.cpp
+++ opencv_win_vc/opencv_win_vc.cpp
@@ -11,12 +11,26 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Executing simple opencv Task!\n";
+	cout << "load image" << endl;
+	Mat img = imread("images/bicycle.png");
+	if (img.empty()) {
+		//failed to load
+		cout << "bicycle image not found." << endl;
+		return -1;
+	}
+
+	String windowName = "Biycle";
+	namedWindow(windowName);
+	imshow(windowName, img);
+
+	// generate a window
 	Mat image = Mat::zeros(300, 600, CV_8UC3);
 	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
 	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
 	imshow("Display Window", image);
+
 	waitKey(0);
+	destroyWindow(windowName);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
