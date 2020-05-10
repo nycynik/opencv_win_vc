ebc1bcb4f81fc0b548d9364bd268bd9b890f47ec
Mike Lynch <nycynik@gmail.com>
2020-05-09 23:45:35 -0700

    removed catch2, replaced with google tests

--- opencv_win_vc/MyOCV_ImageIO.hpp
+++ opencv_win_vc/MyOCV_ImageIO.hpp
@@ -1,9 +1,30 @@
 #pragma once
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
 class MyOCV_ImageIO
 {
-	public:
-		static int test();
+public:
+	static int test();
 
+	// Save and load images
+	static int loadImage(Mat& img, String pathname);
+	static int saveImage(Mat img, String pathname);
+
+	// Image sizing and scaling
+	static int cropImage(Mat& dst, Mat src, long x, long y, long width, long height);
+
+	// Image color manipulation
+	static Mat reduce(Mat& img, int divWidth); 
+
+private:
+	static Mat& scanImageAndReduce(Mat& I, const uchar* const table);
 };
 
 
--- opencv_win_vc/opencv_image_utils.cpp
+++ opencv_win_vc/opencv_image_utils.cpp
@@ -5,160 +5,44 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include "MyOCV_ImageIO.hpp"
+#include "opencv_image_utils.h"
 
 using namespace cv;
 using namespace std;
 
-// fastest way to scan an image and do some operation on it
-Mat& ScanImageAndReduceC(Mat& I, const uchar* const table)
-{
-	// accept only char type matrices
-	CV_Assert(I.depth() == CV_8U);
-	int channels = I.channels();
-	int nRows = I.rows;
-	int nCols = I.cols * channels;
-	if (I.isContinuous())
-	{
-		nCols *= nRows;
-		nRows = 1;
-	}
-	int i, j;
-	uchar* p;
-	for (i = 0; i < nRows; ++i)
-	{
-		p = I.ptr<uchar>(i);
-		for (j = 0; j < nCols; ++j)
-		{
-			p[j] = table[p[j]];
-		}
-	}
-	return I;
-}
 
-// slightly safer way than the C way
-Mat& ScanImageAndReduceIterator(Mat& I, const uchar* const table)
-{
-	// accept only char type matrices
-	CV_Assert(I.depth() == CV_8U);
-	const int channels = I.channels();
-	switch (channels)
-	{
-	case 1:
-	{
-		MatIterator_<uchar> it, end;
-		for (it = I.begin<uchar>(), end = I.end<uchar>(); it != end; ++it)
-			*it = table[*it];
-		break;
-	}
-	case 3:
-	{
-		MatIterator_<Vec3b> it, end;
-		for (it = I.begin<Vec3b>(), end = I.end<Vec3b>(); it != end; ++it)
-		{
-			(*it)[0] = table[(*it)[0]];
-			(*it)[1] = table[(*it)[1]];
-			(*it)[2] = table[(*it)[2]];
-		}
-	}
-	}
-	return I;
-}
-
-// really slow way, but shows random access, in case we need that later.
-Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table)
-{
-	// accept only char type matrices
-	CV_Assert(I.depth() == CV_8U);
-	const int channels = I.channels();
-	switch (channels)
-	{
-	case 1:
-	{
-		for (int i = 0; i < I.rows; ++i)
-			for (int j = 0; j < I.cols; ++j)
-				I.at<uchar>(i, j) = table[I.at<uchar>(i, j)];
-		break;
-	}
-	case 3:
-	{
-		Mat_<Vec3b> _I = I;
-		for (int i = 0; i < I.rows; ++i)
-			for (int j = 0; j < I.cols; ++j)
-			{
-				_I(i, j)[0] = table[_I(i, j)[0]];
-				_I(i, j)[1] = table[_I(i, j)[1]];
-				_I(i, j)[2] = table[_I(i, j)[2]];
-			}
-		I = _I;
-		break;
-	}
-	}
-	return I;
-}
-
-Mat greyScale(Mat& img, int divWidth) {
-	if (divWidth == 0) {
-		cout << "Invalid number entered for dividing. " << endl;
-		return img;
-	}
-
-	// Create a lookup table so that we don't have to * and / each pixel value
-	uchar table[256];
-	for (int i = 0; i < 256; ++i)
-		table[i] = (uchar)(divWidth * (i / divWidth));
-
-	// start timer
-	double t = (double)getTickCount();
-	
-	Mat img_grey = ScanImageAndReduceC(img, table);
-
-	// show how long it took
-	t = ((double)getTickCount() - t) / getTickFrequency();
-	cout << "c method in seconds               : " << t << endl;
-
-
-	t = (double)getTickCount();
-	img_grey = ScanImageAndReduceIterator(img, table);
-	t = ((double)getTickCount() - t) / getTickFrequency();
-	cout << "iterator method passed in seconds : " << t << endl;
-
-
-	t = (double)getTickCount();
-	img_grey = ScanImageAndReduceRandomAccess(img, table);
-	t = ((double)getTickCount() - t) / getTickFrequency();
-	cout << "row col method passed in seconds  : " << t << endl;
-
-
-	return img_grey;
-
-}
 int main()
 {
 	cout << "load image" << endl;
-	Mat img = imread("images/bicycle.png");
-	if (img.empty()) {
-		//failed to load
-		cout << "bicycle image not found." << endl;
-		return -1;
+	Mat img;
+	String windowName;
+	
+	if (MyOCV_ImageIO::loadImage(img, "images/bicycle.png") == 0) {
+
+		Mat img2;
+		MyOCV_ImageIO::cropImage(img2, img, 10, 130, 140, 180);
+
+		windowName = "Biycle";
+		namedWindow(windowName);
+		imshow(windowName, img2);
 	}
-
-	// x, y, width, height
-	cv::Rect wheelROI(10, 130, 140, 180);
-	Mat img2 = img(wheelROI);
-
-	String windowName = "Biycle";
-	namedWindow(windowName);
-	imshow(windowName, img2);
 
 	Mat img3 = imread("images/fruit.png");
 	windowName = "Original Fruit";
 	namedWindow(windowName);
 	imshow(windowName, img3);
 
-	Mat img_grey = greyScale(img3, 86);
+	// start timer
+	double t = (double)getTickCount();	
+	Mat img_grey = MyOCV_ImageIO::reduce(img3, 84);
 	windowName = "Reduced Fruit";
 	namedWindow(windowName);
 	imshow(windowName, img3);
+
+	// show how long it took
+	t = ((double)getTickCount() - t) / getTickFrequency();
+	cout << "c method in seconds               : " << t << endl;
 
 	// generate a window
 	Mat image = Mat::zeros(300, 600, CV_8UC3);
@@ -167,15 +51,13 @@ int main()
 	imshow("Display Window", image);
 
 	// save a file
-	bool isSaved = imwrite("./MyImage.jpg", img3); // save as JPEG, we can use other filenames to save as alt formats, like .png
+	if (MyOCV_ImageIO::saveImage(img3, "./MyImage.jpg") != 0) { // save as JPEG, we can use other filenames to save as alt formats, like .png
 	
-	if (isSaved == false) {
-
 		cout << "Failed to save the image" << endl;
 		cin.get(); //wait for a key press
-	}
-	else {
-		cout << "Image is succusfully saved to a file" << endl;
+
+	} else {
+		cout << "Image was saved to a file" << endl;
 	}
 	
 
